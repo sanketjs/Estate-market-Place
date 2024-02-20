9295e2a8c8e63cd6af3db6d57c8ff684854f9bc9
sanketjs <sanket.kere28@gmail.com>
2024-02-20 22:42:12 +0530

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
+  return(
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path='/' element={<Home/>}/>
-    <Route path='signin' element={<Signin/>}/>
+    <Route path='sign-in' element={<Signin/>}/>
     <Route path='sign-up' element={<SignUp/>}/>
     <Route path='about' element={<About/>}/>
     <Route path='profile' element={<Profile/>}/>
    </Routes>
    
    </BrowserRouter>
+  );
 }
